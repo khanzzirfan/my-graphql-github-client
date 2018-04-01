1c5a7b77a8421e63b354ffb53d2765547d4e6955
khanzzirfan <khanzz_irfan@hotmail.com>
2018-04-01 14:57:54 +1200

    chor($styles): Applied search button styles

--- src/Header/HeaderComponent.js
+++ src/Header/HeaderComponent.js
@@ -3,43 +3,58 @@ import PropTypes from 'prop-types';
 import "./header.css";
 import githubLogo from "../github1.svg";
 
-const HeaderComponent = ({
-    onProfileClicked,
-    onOrganizationClicked,
-    isProfileSelected
-}) => {
+class HeaderComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        }
+    }
 
-    return (
-        <div className="row justify-content-center">
-            <div className="row ml-4 justify-content-end">
-                <div className="col-lg-12">
-                    <div className="input-group">
-                        <span className="input-group-btn">
-                            <img src={githubLogo} className="github-logo" alt="logo" />
-                        </span>
-                        <input type="text" className="form-control" placeholder="search github" />
+    handleDropDown= () => {
+        this.setState({show: true});
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setState({show: false});
+    }
+
+    render() {
+        const { onProfileClicked,
+            onOrganizationClicked,
+            isProfileSelected } = this.props;
+
+        let dropdownText = isProfileSelected === true ? "Profile" : "Organization";
+        return (
+            <div className="row justify-content-center">
+                <div className="row ml-4 justify-content-end">
+                    <div className="col-lg-12">
+                        <div className="input-group">
+                            <span className="input-group-btn">
+                                <img src={githubLogo} className="github-logo" alt="logo" />
+                            </span>
+                            <input type="text" className="form-control" placeholder="search github" />
+                            <div className={`input-group-append ${this.state.show ? "show": ""}`}>
+                                <button type="button" className="btn bg-white btn-outline-secondary selected-dropdown">{dropdownText}</button>
+                                <button type="button" className="btn bg-white btn-outline-secondary dropdown-toggle dropdown-toggle-split"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={this.handleDropDown}>
+                                    <span className="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div className={`dropdown-menu ${this.state.show? "show": ""}`}>
+                                    <a className="dropdown-item" href="javascript:void(0)" onClick={onProfileClicked}>Profile</a>
+                                    <a className="dropdown-item" href="javascript:void(0)" onClick={onOrganizationClicked}>Organization</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-lg-3 mx-2">
-                    <div className="input-group mx-2">
-                        <span className="input-group-btn">
-                            <button className={`btn ${isProfileSelected == true ? "btn-primary" : "btn-secondary"}`} type="button" onClick={onProfileClicked}>Profile</button>
-                        </span>
-                    </div>
-                </div>
-                <div className="col-lg-5 mx-2">
-                    <div className="input-group">
-                        <span className="input-group-btn">
-                            <button className={`btn ${isProfileSelected == false ? "btn-primary" : "btn-secondary"}`} type="button" onClick={onOrganizationClicked}>Organization</button>
-                        </span>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    );
+        );
+    }
 }
+
+HeaderComponent.propTypes = {
+
+};
 
 export default HeaderComponent;
